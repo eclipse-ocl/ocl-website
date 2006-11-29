dad116f4a5a08993ad44c5cf2d1391d19bb2267b
nickb <nickb>
2006-11-29 01:34:59 +0000

    only show extras if project matches

--- news/relnotes.php
+++ news/relnotes.php
@@ -1,19 +1,25 @@
 <?php
+
 /* projects/components in cvs */
 /* "proj" => "cvsname" */
-$cvsprojs = array(
+$cvsprojs = array (
 	"uml2-uml" => "org.eclipse.uml2",
 	"xsd" => "org.eclipse.xsd"
 );
 
 /* sub-projects/components in cvs for projects/components above (if any) */
 /* "cvsname" => array("shortname" => "cvsname") */
-$cvscoms = array(
-	"org.eclipse.emft" => array(
+$cvscoms = array (
+	"org.eclipse.emft" => array (
 		"uml2-ocl" => "ocl",
 		"eodm" => "eodm"
 	)
 );
 
-require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/relnotes-common.php");
+if (isset ($_GET["project"]) && $_GET["project"] == "xsd")
+{
+	include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/mdt/xsd/news/relnotes-common-xsd.php";
+}
+
+require ($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/relnotes-common.php");
 ?>
