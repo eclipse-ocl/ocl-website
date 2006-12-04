b9dbf7637c6da4b61b456c1f2f4b971ec1d4506d
nickb <nickb>
2006-12-04 23:11:49 +0000

    {skrypuch} relnotes_sidebar patches

---
+++ xsd/news/relnotes-extras.php
@@ -0,0 +1,16 @@
+<?php
+function sideitems()
+{
+	return <<<XML
+	<div class="sideitem">
+		<h6>Releases</h6>
+		<p>
+			<ul>
+				<li><a href="http://www.eclipse.org/modeling/emf/news/relnotes.php?project=xsd">Latest</a></li>
+				<li><a href="http://www.eclipse.org/modeling/emf/xsd/news/relnotes2.0.php#xsd">XSD 2.0</a></li>
+				<li><a href="http://www.eclipse.org/modeling/emf/xsd/news/relnotes1.x.php#xsd">XSD 1.x</a></li>
+			</ul>
+		</p>
+	</div>
+XML;
+}
