83eea966f003b5d6d249304fac3f34dc3314fc5a
nickb <nickb>
2007-01-16 23:25:50 +0000

    [170668] temporary hack

--- news/relnotes.php
+++ news/relnotes.php
@@ -2,5 +2,12 @@
 
 $pageTitle = "Eclipse Modeling - MDT - Release Notes";
 
+// temporary hack
+if (isset($_GET["project"]))
+{
+	if ($_GET["project"] == "uml2") { $_GET["project"] = "uml2-uml"; }
+	if ($_GET["project"] == "ocl")  { $_GET["project"] = "uml2-ocl"; }
+}
+
 require ($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/relnotes-common.php");
 ?>
