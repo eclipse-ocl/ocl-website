ac3a47f6809a505acb60f103638ea3e592267e19
nickb <nickb>
2007-02-08 16:56:10 +0000

    redirect until this page is done

--- uml2/build/promo.php
+++ uml2/build/promo.php
@@ -1,6 +1,11 @@
 <?php
+
+/*
 require_once ("_common.php");
 
 #TODO move this up one level into /modeling/
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/mdt/build/promo-common.php");
+*/
+/* redirect to working page */
+header("Location: /emf/build/promo.php?project=uml2");
 ?>
