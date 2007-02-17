ff75061446c8bd1a2a48d5ee1636df74be0a778c
nickb <nickb>
2007-02-17 22:21:27 +0000

    disable promo pages

--- ocl/build/promo.php
+++ ocl/build/promo.php
@@ -1,6 +1,9 @@
 <?php
+/*
 require_once ("_common.php");
 
 #TODO move this up one level into /modeling/
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/mdt/build/promo-common.php");
+*/
+print "Coming soon.";
 ?>
