0bc111495d552b44abe4f8dc9be89a2a229f64f5
nickb <nickb>
2007-02-17 07:52:36 +0000

    add EODM

--- build/build-common.php
+++ build/build-common.php
@@ -7,7 +7,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");  require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
 
 // temporarily suppress unsupported projects
-$nodownloads = array ("xsd", "eodm");  
+$nodownloads = array ("xsd");  
 
 internalUseOnly(); 
 ob_start();
