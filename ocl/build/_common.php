2f22f52de2f87c5acf188789b7663ec8fb6ad5c5
nickb <nickb>
2007-03-01 16:55:25 +0000

    remove unneeded build targets

--- ocl/build/_common.php
+++ ocl/build/_common.php
@@ -4,7 +4,7 @@ $options = array (
 	
 	"BranchAndJDK" => array (
 		"1.1.0=HEAD,/opt/sun-java2-5.0",
-		"1.0.2=R1_0_maintenance,/opt/sun-java2-1.4"
+		#"1.0.3=R1_0_maintenance,/opt/sun-java2-1.4"
 	),
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, Tag Files=gen-true"
