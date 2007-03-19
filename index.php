ffadbe03eaf62eb0586c0633ab8c96326f948f9e
nickb <nickb>
2007-03-19 21:30:42 +0000

    [178142] [178148] [171427]

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$_SERVER['DOCUMENT_ROOT'] = $_SERVER["SERVER_NAME"] == "build.eclipse.org" ? "/opt/public/modeling" : $_SERVER['DOCUMENT_ROOT'];
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
 
 require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/db.php");
@@ -151,4 +152,4 @@ $pageAuthor = "Neil Skrypuch";
 $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/index.css"/>' . "\n");
 $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
-<!-- $Id: index.php,v 1.20 2007/03/06 22:38:56 nickb Exp $ -->
+<!-- $Id: index.php,v 1.21 2007/03/19 21:27:38 nickb Exp $ -->
