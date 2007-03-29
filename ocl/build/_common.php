a91ccdd52e54fcd77a1ba1f21c358b09479b63a1
nickb <nickb>
2007-03-29 23:09:39 +0000

    [178148] add build.eclipse options

--- ocl/build/_common.php
+++ ocl/build/_common.php
@@ -5,8 +5,13 @@ $options = array (
 	"BaseBuilderBranch" => "M4_33",
 	
 	"BranchAndJDK" => array (
+		"emf.torolab.ibm.com=------------,------------",
 		"1.1.0=HEAD,/opt/sun-java2-5.0",
-		#"1.0.3=R1_0_maintenance,/opt/sun-java2-1.4"
+		#"1.0.3=R1_0_maintenance,/opt/sun-java2-1.4",
+		
+		"build.eclipse.org=------------,------------",
+		"1.1.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
+		#"1.0.3=R1_0_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // bug 178681
 	),
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
