7742e1c41b2b006923b949f7da659609a3d27c68
nickb <nickb>
2007-03-30 00:27:35 +0000

    [176666]

--- news-whatsnew.php
+++ news-whatsnew.php
@@ -1,4 +1,5 @@
 <?php
+require_once ("../includes/buildServer-common.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/news-common.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/db.php");
 
