e89b002bc46682a8392f04f544ceda4cf99ede0f
nickb <nickb>
2007-04-20 20:30:58 +0000

    fix link

--- faq.php
+++ faq.php
@@ -11,7 +11,7 @@ ob_start();
 <div id="midcolumn">
 
 <h1>Frequently Asked Questions</h1>
-<p>If you have questions that you would like to see answered in future versions of this FAQ, please post them to the <a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.modeling.mdt">MDT newsgroup</a>.</p>
+<p>If you have questions that you would like to see answered in future versions of this FAQ, please post them to the <a href="newsgroup-mailing-list.php">newsgroup</a>.</p>
 
 <p>If you'd like to add your questions and answers to the FAQ, please edit the appropriate <a href="http://wiki.eclipse.org/index.php/MDT">Wiki page</a>.</p>
 
@@ -30,4 +30,4 @@ $pageAuthor = "Neil Skrypuch";
 
 $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
-<!-- $Id: faq.php,v 1.7 2007/03/30 19:00:52 nickb Exp $ -->
+<!-- $Id: faq.php,v 1.8 2007/04/20 20:30:58 nickb Exp $ -->
