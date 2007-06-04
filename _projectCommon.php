bbd3726e7fcdd04bccfc54cdd77da6bf613f1bcc
nickb <nickb>
2007-06-04 16:35:39 +0000

    {skrypuch} move $writableRoot into db.php, since that's the only place it's used

--- _projectCommon.php
+++ _projectCommon.php
@@ -10,7 +10,6 @@ $isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
 $isWWWserver = (preg_match("/^(?:www.|)eclipse.org$/", $_SERVER["SERVER_NAME"]));
 $isEclipseCluster = (preg_match("/^(?:www.||download.|download1.|build.)eclipse.org$/", $_SERVER["SERVER_NAME"]));
 $debug = (isset ($_GET["debug"]) && preg_match("/^\d+$/", $_GET["debug"]) ? $_GET["debug"] : -1);
-$writableRoot = ($isBuildServer ? $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/" : "/home/data/httpd/writable/www.eclipse.org/");
 $writableBuildRoot = $isBuildDotEclipseServer ? "/opt/public/modeling" : "/home/www-data";
 
 $rooturl = "http://" . $_SERVER["HTTP_HOST"] . "/$PR";
