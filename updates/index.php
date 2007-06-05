968ef9e396405434af17766b16d76988a82bf8a3
nickb <nickb>
2007-06-05 19:38:38 +0000

    [191098]

--- updates/index.php
+++ updates/index.php
@@ -11,5 +11,5 @@ function notes()
 	print "<p><i style=\"color:red\"><b>NOTE:</b> not all MDT projects have migrated to the MDT Update Manager site yet, so you'll have to use the older sites until that time.</i></p>\n";
 }
 
-update_manager("MDT", "Eclipse Modeling", $PRS);
+update_manager("MDT", "Eclipse Modeling", $PRS, true);
 ?>
