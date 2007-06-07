b04d80f44158ffd8cde1468584ea2855ed964fa9
nickb <nickb>
2007-06-07 15:38:00 +0000

    *** empty log message ***

--- uml2tools/build/_common.php
+++ uml2tools/build/_common.php
@@ -9,7 +9,7 @@ $options = array (
 		"0.8.0=HEAD,/opt/sun-java2-5.0",
 		
 		"build.eclipse.org=------------,------------",
-		"0.9.0=HEAD,/opt/public/common/ibm-java2-ppc-50"
+		"0.8.0=HEAD,/opt/public/common/ibm-java2-ppc-50"
 	),
 	
 	"Mapfile_Rule_Default" => 1, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
