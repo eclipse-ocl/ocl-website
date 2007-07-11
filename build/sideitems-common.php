06bb31955b38862a076940730de58f3710d05c03
nickb <nickb>
2007-07-11 04:25:18 +0000

    remove old build links

--- build/sideitems-common.php
+++ build/sideitems-common.php
@@ -10,17 +10,15 @@ function sidebar()
 	<div class="sideitem">
 		<h6>Actions</h6>
 		<ul>
+		<?php if ($projct == "ocl") { ?> 
 			<li>MDT: <a href="/modeling/mdt/<?php print $projct; ?>/build/">Build</a>, 
 					 <a href="/modeling/mdt/<?php print $projct; ?>/build/promo.php">Promote</a></li>
-			<li>QTV: <a href="/modeling/emf/build/">Build</a>, 
-					 <a href="/modeling/emf/build/promo.php">Promote</a></li>
-		</ul>
-		<hr noshade="noshade" size="1" width="80%"/>
-		<ul>
-			<li><a href="/emft/build/?project=<?php print $projct; ?>">Old Build</a> (EMFT)</li>
-			<li><a href="http://emf.torolab.ibm.com/emf/build/?project=emf">Old Build</a> (XSD)</li>
-			<li><a href="http://emf.torolab.ibm.com/emf/build/?project=uml2">Old Build</a> (UML2)</li>
-			<li><a href="http://emf.torolab.ibm.com/emf/build/promo.php?project=uml2">Old Promote</a> (UML2)</li>
+			<li>QTV: <a href="/modeling/emf/query/build/">Build</a>, 
+					 <a href="/modeling/emf/query/build/promo.php">Promote</a></li>
+		<?php } else { ?>
+			<li><a href="/modeling/mdt/<?php print $projct; ?>/build/">Build</a>, 
+					 <a href="/modeling/mdt/<?php print $projct; ?>/build/promo.php">Promote</a></li>
+		<?php } ?>
 		</ul>
 	</div>
 
