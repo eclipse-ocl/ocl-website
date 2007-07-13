a29eaa172b299dfeaee031a0353c72a35c03b72b
nickb <nickb>
2007-07-13 00:04:10 +0000

    [192773] no need to trap project= value for emf/sdo

--- xsd/versionaudit.php
+++ xsd/versionaudit.php
@@ -1,15 +1,11 @@
 <?php
 
-$dirs = array();
-if (isset($_GET["project"]) && $_GET["project"] == "xsd")
-{
-	$dirs = array(
-		"HEAD" => "/opt/public/modeling/searchcvs/cvssrc/org.eclipse.xsd",
-		"R2_2_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.xsd-R2_2_maintenance",
-		"R2_1_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.xsd-R2_1_maintenance",
-		"R2_0_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.xsd-R2_0_maintenance"
-	);
-}
+$dirs = array(
+	"HEAD" => "/opt/public/modeling/searchcvs/cvssrc/org.eclipse.xsd",
+	"R2_2_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.xsd-R2_2_maintenance",
+	"R2_1_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.xsd-R2_1_maintenance",
+	"R2_0_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.xsd-R2_0_maintenance"
+);
 
 include($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/versionaudit-common.php");
 ?>
