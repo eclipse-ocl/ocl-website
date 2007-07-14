99259db9a2774232da4890a9348f5f5076d16ac1
nickb <nickb>
2007-07-14 21:51:13 +0000

    [196412] move into new paths

--- xsd/versionaudit.php
+++ xsd/versionaudit.php
@@ -2,10 +2,10 @@
 require_once ("../../includes/buildServer-common.php");
 
 $dirs = array(
-	"HEAD" => "/opt/public/modeling/searchcvs/cvssrc/org.eclipse.xsd",
-	"R2_2_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.xsd-R2_2_maintenance",
-	"R2_1_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.xsd-R2_1_maintenance",
-	"R2_0_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/org.eclipse.xsd-R2_0_maintenance"
+	"HEAD" => "/opt/public/modeling/searchcvs/cvssrc/mdt-org.eclipse.xsd",
+	"R2_2_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/mdt-org.eclipse.xsd-R2_2_maintenance",
+	"R2_1_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/mdt-org.eclipse.xsd-R2_1_maintenance",
+	"R2_0_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/mdt-org.eclipse.xsd-R2_0_maintenance"
 );
 
 include_once($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/versionaudit-common.php");
