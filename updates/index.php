fb314d10942fb9fd270edcc6ed3e91fc5c2f04bd
nickb <nickb>
2007-07-18 20:25:20 +0000

    [196717] bounce from mirrors to www.eclipse.org

--- updates/index.php
+++ updates/index.php
@@ -1,4 +1,7 @@
 <?php 
+if ($_SERVER["SERVER_NAME"] != "www.eclipse.org") {
+	header("Location: http://www.eclipse.org/modeling/mdt/updates/");
+}
 require_once ("../../includes/buildServer-common.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/updates-common.php");
 
