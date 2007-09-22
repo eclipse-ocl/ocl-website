59e94b4f8de72ff6ba4533e9264f32d8cc4c776b
nickb <nickb>
2007-09-22 06:28:22 +0000

    [194263] also, clean up projectCommon files and make more consistent between 4 versions

--- _projectCommon.php
+++ _projectCommon.php
@@ -3,6 +3,7 @@
 $Nav->setLinkList(null);
 
 $PR = "modeling/mdt";
+$projectName = "MDT";
 
 $isEMFserver = (preg_match("/^emf(?:\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"]));
 $isBuildServer = (preg_match("/^(emft|build)\.eclipse\.org$/", $_SERVER["SERVER_NAME"])) || $isEMFserver;
@@ -10,6 +11,7 @@ $isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
 $isWWWserver = (preg_match("/^(?:www.|)eclipse.org$/", $_SERVER["SERVER_NAME"]));
 $isEclipseCluster = (preg_match("/^(?:www.||download.|download1.|build.)eclipse.org$/", $_SERVER["SERVER_NAME"]));
 $debug = (isset ($_GET["debug"]) && preg_match("/^\d+$/", $_GET["debug"]) ? $_GET["debug"] : -1);
+$writableRoot = ($isBuildServer ? $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/" : "/home/data/httpd/writable/www.eclipse.org/");
 $writableBuildRoot = $isBuildDotEclipseServer ? "/opt/public/modeling" : "/home/www-data";
 
 $rooturl = "http://" . $_SERVER["HTTP_HOST"] . "/$PR";
@@ -27,7 +29,7 @@ else
 
 /* projects/components in cvs */
 /* "proj" => "cvsname" */
-$cvsprojs = array ();
+$cvsprojs = array (); /* should always be empty */
 
 /* sub-projects/components in cvs for projects/components above (if any) */
 /* "cvsname" => array("shortname" => "cvsname") */
@@ -49,28 +51,21 @@ $projects = array (
 	"UML2 Tools" => "uml2tools",
 	"XSD" => "xsd"
 );
+$bugcoms = array_flip($projects);
+$bugcoms = preg_replace("/ /", "%20", $bugcoms);
 
-$extraprojects = array (); //projects with only downloads, no info yet, "prettyname" => "directory"
-$nodownloads = array (); //projects with only information, no downloads, or no builds available yet, "projectkey"
-$nonewsgroup = array (); //projects without newsgroup
-$nomailinglist = array (); //projects without mailinglist
-$incubating = array("eodm","uml2tools"); // projects which are still incubating
-
+$extraprojects = array(); //components with only downloads, no info yet, "prettyname" => "directory"
+$nodownloads = array(); //components with only information, no downloads, or no builds available yet, "projectkey"
+$nonewsgroup = array(); //components without newsgroup
+$nomailinglist = array(); //components without mailinglist
+$incubating = array("eodm","uml2tools"); // components which are still incubating
 $nomenclature = "Component"; //are we dealing with "components" or "projects"?
 
 include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/scripts.php";
 
 $regs = null;
 $proj = (isset($_GET["project"]) && preg_match("/^(" . join("|", $projects) . ")$/", $_GET["project"], $regs) ? $regs[1] : getProjectFromPath($PR));
-
-$Nav->addNavSeparator("MDT", "$rooturl/");
-foreach (array_keys($projects) as $z)
-{
-	$Nav->addCustomNav($z, "$rooturl/?project=$projects[$z]", "_self", 2);
-}
-
-$bugcoms = array_flip($projects);
-$bugcoms = preg_replace("/ /", "%20", $bugcoms);
+$projct= preg_replace("#^/#", "", $proj);
 
 $buildtypes = array(
 	"R" => "Release",
@@ -80,6 +75,12 @@ $buildtypes = array(
 	"N" => "Nightly"
 );
 
+$Nav->addNavSeparator($projectName, "$rooturl/");
+foreach (array_keys(array_diff($projects, $extraprojects)) as $z)
+{
+	$Nav->addCustomNav($z, "$rooturl/?project=$projects[$z]", "_self", 2);
+}
+
 $Nav->addNavSeparator("Downloads", "$downurl/$PR/downloads/?project=$proj");
 $Nav->addCustomNav("Update Manager", "$rooturl/updates/", "_self", 2);
 
@@ -88,15 +89,15 @@ $Nav->addCustomNav("FAQ", "$rooturl/faq.php?project=$proj", "_self", 2);
 $Nav->addCustomNav("Plan", "$rooturl/docs/plans/mdt_project_plan_1_1.html", "_self", 2);
 
 $Nav->addCustomNav("Release Notes", "http://www.eclipse.org/$PR/news/relnotes.php?project=$proj&amp;version=HEAD", "_self", 2);
-$Nav->addCustomNav("Search CVS", "http://www.eclipse.org/$PR/searchcvs.php?q=file%3A+org.eclipse.mdt%2F" . ($proj?"org.eclipse.".$proj."%2F":"") . "+days%3A+7", "_self", 2);
+$Nav->addCustomNav("Search CVS", "http://www.eclipse.org/$PR/searchcvs.php?q=file%3A+org.eclipse." . strtolower($projectName) . "%2F" . ($proj?"org.eclipse.".$proj."%2F":"") . "+days%3A+7", "_self", 2);
 
-$Nav->addNavSeparator("Community", "http://wiki.eclipse.org/index.php/Modeling_Corner");
-$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/".($proj?"MDT-" . strtoupper($proj):"Model_Development_Tools_%28MDT%29"), "_self", 2);
+$Nav->addNavSeparator("Community", "http://wiki.eclipse.org/Modeling_Corner");
+$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/" . ($proj?$projectName . "-" . strtoupper($proj):$projectName), "_self", 2);
 $Nav->addCustomNav("Newsgroups", "$rooturl/newsgroup-mailing-list.php", "_self", 2);
-$Nav->addCustomNav("Modeling Corner", "http://wiki.eclipse.org/index.php/Modeling_Corner", "_self", 2);
+$Nav->addCustomNav("Modeling Corner", "http://wiki.eclipse.org/Modeling_Corner", "_self", 2);
 $collist = "%26query_format%3Dadvanced&amp;column_changeddate=on&amp;column_bug_severity=on&amp;column_priority=on&amp;column_rep_platform=on&amp;column_bug_status=on&amp;column_product=on&amp;column_component=on&amp;column_version=on&amp;column_target_milestone=on&amp;column_short_short_desc=on&amp;splitheader=0";
-$Nav->addCustomNav("Open Bugs", "$bugurl/bugs/colchange.cgi?rememberedquery=product%3DMDT" . (isset ($bugcoms[$proj]) ? "%26component=$bugcoms[$proj]" : "") . "%26bug_status%3DNEW%26bug_status%3DASSIGNED%26bug_status%3DREOPENED%26order%3Dbugs.bug_status%2Cbugs.target_milestone%2Cbugs.bug_id" . $collist, "_self", 2);
-$Nav->addCustomNav("Submit A Bug", "$bugurl/bugs/enter_bug.cgi?product=MDT" . (isset ($bugcoms[$proj]) ? "&amp;component=$bugcoms[$proj]" : ""), "_self", 2);
+$Nav->addCustomNav("Open Bugs", "$bugurl/bugs/colchange.cgi?rememberedquery=product%3D" . $projectName . (isset ($bugcoms[$proj]) ? "%26component=$bugcoms[$proj]" : "") . "%26bug_status%3DNEW%26bug_status%3DASSIGNED%26bug_status%3DREOPENED%26order%3Dbugs.bug_status%2Cbugs.target_milestone%2Cbugs.bug_id" . $collist, "_self", 2);
+$Nav->addCustomNav("Submit A Bug", "$bugurl/bugs/enter_bug.cgi?product=" . $projectName . (isset ($bugcoms[$proj]) ? "&amp;component=$bugcoms[$proj]" : ""), "_self", 2);
 $Nav->addCustomNav("Contributors", "$rooturl/project-info/team.php", "_self", 2);
 
 $App->AddExtraHtmlHeader("<link rel=\"stylesheet\" type=\"text/css\" href=\"/modeling/includes/common.css\"/>\n");
