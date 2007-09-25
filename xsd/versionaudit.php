bebed101976521151af4dc0f1081ab14b29c15e7
nickb <nickb>
2007-09-25 15:45:39 +0000

    add R2_3_maintenance

--- xsd/versionaudit.php
+++ xsd/versionaudit.php
@@ -3,6 +3,7 @@ require_once ("../../includes/buildServer-common.php");
 
 $dirs = array(
 	"HEAD" => "/opt/public/modeling/searchcvs/cvssrc/mdt-org.eclipse.xsd",
+	"R2_3_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/mdt-org.eclipse.xsd-R2_3_maintenance",
 	"R2_2_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/mdt-org.eclipse.xsd-R2_2_maintenance",
 	"R2_1_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/mdt-org.eclipse.xsd-R2_1_maintenance",
 	"R2_0_maintenance" => "/opt/public/modeling/searchcvs/cvssrc_branches/mdt-org.eclipse.xsd-R2_0_maintenance"
