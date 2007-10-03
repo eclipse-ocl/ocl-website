953533287c31ba08b16214e55fa940a419e145fc
nickb <nickb>
2007-10-03 06:12:51 +0000

    [194263] allow static lists of bugs per build

--- xsd/news/relnotes-extras.php
+++ xsd/news/relnotes-extras.php
@@ -3,4 +3,18 @@ $additionalSideItems = '
 	<li><a href="http://www.eclipse.org/' . $PR . '/' . $projct . '/news/relnotes2.0.php">2.0.0</a></li>
 	<li><a href="http://www.eclipse.org/' . $PR . '/' . $projct . '/news/relnotes1.x.php">1.x</a></li>
 ';
+
+$cannedBugs = array(
+	"2.3.1RC" => array("200421"),
+	"M200709120130" => array("202969", "201876"),
+	"M200709042200" => array(),
+	"M200708301145" => array(),
+	"M200708282230" => array(),
+	"M200708142030" => array("199540", "198803"),
+	"M200708072030" => array(),
+	"M200707312030" => array(),
+	"M200707242120" => array("197693", "99649"),
+	"M200707171853" => array("196408", "192773"),
+	"M200707111516" => array("195975", "195288")
+);
 ?>
