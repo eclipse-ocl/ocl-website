b3c96fbaf3bd71c2768c08417cb14d9bb5ac1d54
mgolubev <mgolubev>
2007-10-03 16:46:05 +0000

    updated to 0.8.0 stream

--- uml2tools/build/_common.php
+++ uml2tools/build/_common.php
@@ -6,10 +6,10 @@ $options = array (
 	
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
-		"0.7.1=HEAD,/opt/sun-java2-5.0",
+		"0.8.0=HEAD,/opt/sun-java2-5.0",
 		
 		"build.eclipse.org=------------,------------",
-		"0.7.1=HEAD,/opt/public/common/ibm-java2-ppc-50"
+		"0.8.0=HEAD,/opt/public/common/ibm-java2-ppc-50"
 	),
 	
 	"Mapfile_Rule_Default" => 1, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
