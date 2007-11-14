b678fdb79e5efc21222a1a30d7e75a9a59e8bbe3
cdamus <cdamus>
2007-11-14 20:12:51 +0000

    Use M1_34 basebuilder by default

--- ocl/build/_common.php
+++ ocl/build/_common.php
@@ -2,7 +2,7 @@
 require_once ("../../../includes/buildServer-common.php");
 
 $options = array (
-	"BaseBuilderBranch" => "v20070614",
+	"BaseBuilderBranch" => "M1_34",
 	
 	"BranchAndJDK" => array (
 		"emf.torolab.ibm.com=------------,------------",
