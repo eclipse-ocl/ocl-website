6a78070fa77c8b0a3405896f44ea190f4c768d3c
nickb <nickb>
2008-01-21 21:17:08 +0000

    [206558] add links for RESOLVED in addition to ASSIGNED

--- build/sideitems-common.php
+++ build/sideitems-common.php
@@ -27,6 +27,7 @@ function sidebar()
 		<h6>Info</h6>
 		<ul>
 			<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=MDT&amp;component=<?php echo $projct; ?>&amp;bug_status=ASSIGNED">Assigned Bugs</a></li>
+			<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=MDT&amp;component=<?php echo $projct; ?>&amp;bug_status=RESOLVED">Resolved Bugs</a></li>
 			<li><a href="http://www.eclipse.org/modeling/mdt/searchcvs.php?q=branch%3A+HEAD+days%3A+7">Development This Week</a></li>
 			<li><a href="http://www.eclipse.org/modeling/mdt/searchcvs.php?q=branch%3A+R+days%3A+7">Maintenance This Week</a></li>
 			<!-- <li><a href="http://emf.torolab.ibm.com/<?php print $PR; ?>/downloads/downloads.php">Download Stats</a></li> -->
