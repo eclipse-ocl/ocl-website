c78b89d17e611daf0459445df190849ad7fa8855
mgolubev <mgolubev>
2008-02-05 19:15:11 +0000

    [217829] - maintenance stream added (CVS branch name corrected)

--- uml2tools/build/_common.php
+++ uml2tools/build/_common.php
@@ -7,12 +7,12 @@ $options = array (
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
 		"0.8.0=HEAD,/opt/sun-java2-5.0",
-		"0.7.0=R0_7_0_maintenance,/opt/sun-java2-5.0",
+		"0.7.0=R0_7_0_Maintenance,/opt/sun-java2-5.0",
 		
 		
 		"build.eclipse.org=------------,------------",
 		"0.8.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
-		"0.7.0=R0_7_0_maintenance,/opt/public/common/ibm-java2-ppc-50"
+		"0.7.0=R0_7_0_Maintenance,/opt/public/common/ibm-java2-ppc-50"
 	),
 	
 	"Mapfile_Rule_Default" => 1, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
