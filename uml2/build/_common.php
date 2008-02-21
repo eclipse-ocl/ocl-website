dcf65f0a2475775d4ed281e616872dab73d669c8
nickb <nickb>
2008-02-21 16:05:51 +0000

    open 2.0.5

--- uml2/build/_common.php
+++ uml2/build/_common.php
@@ -8,12 +8,12 @@ $options = array (
 		"emf.torolab.ibm.com=------------,------------",
 		"2.2.0=HEAD,/opt/sun-java2-5.0",
 		"2.1.1=R2_1_maintenance,/opt/sun-java2-5.0",
-		"2.0.4=R2_0_maintenance,/opt/sun-java2-1.4",
+		"2.0.5=R2_0_maintenance,/opt/sun-java2-1.4",
 
 		"build.eclipse.org=------------,------------",
 		"2.2.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
 		"2.1.1=R2_1_maintenance,/opt/public/common/ibm-java2-ppc-50",
-		"2.0.4=R2_0_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // bug 178681
+		"2.0.5=R2_0_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // bug 178681
 	),
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
