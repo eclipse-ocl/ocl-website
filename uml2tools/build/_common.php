699b31261ae6f4bfdb9ee64aae4d87f42ed130db
nickb <nickb>
2008-02-22 02:16:00 +0000

    step up to 0.7.2

--- uml2tools/build/_common.php
+++ uml2tools/build/_common.php
@@ -3,22 +3,22 @@ require_once ("../../../includes/buildServer-common.php");
 
 $options = array (
 	"BaseBuilderBranch" => "M4_34",
-	
+
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
 		"0.8.0=HEAD,/opt/sun-java2-5.0",
-		"0.7.1=R0_7_0_Maintenance,/opt/sun-java2-5.0",
-		
-		
+		"0.7.2=R0_7_0_Maintenance,/opt/sun-java2-5.0",
+
+
 		"build.eclipse.org=------------,------------",
 		"0.8.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
-		"0.7.1=R0_7_0_Maintenance,/opt/public/common/ibm-java2-ppc-50"
+		"0.7.2=R0_7_0_Maintenance,/opt/public/common/ibm-java2-ppc-50"
 	),
-	
+
 	"Mapfile_Rule_Default" => 1, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
 	"EmailDefault" => "michael.golubev@borland.com", // prefil email contact box with comma-sep'd list
-	
+
 	"Users" => array ("mgolubev","mgolubev",null) /* build user, eclipse cvs user, IES cvs user */
 );
 
