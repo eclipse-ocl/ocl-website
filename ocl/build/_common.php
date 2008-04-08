066a5e6e28c4e9ebb34a98a4307f79163aad6cfe
nickb <nickb>
2008-04-08 20:41:17 +0000

    [222298] Make build page default select the regex-defined latest dependencies

--- ocl/build/_common.php
+++ ocl/build/_common.php
@@ -15,6 +15,12 @@ $options = array (
 		"1.1.2=R1_1_maintenance,/opt/public/common/ibm-java2-ppc-50",
 		#"1.0.3=R1_0_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // bug 178681
 	),
+
+	/* define a regular expression to be used to collect the most recent 
+	 * matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
+	 * and /home/www-data/build/requests/dependencies.urls.txt 
+	 * */ 	
+	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-sdo-xsd-SDK-|[ISR]200.*/mdt-uml2-SDK|orbit",
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
--- uml2/build/_common.php
+++ uml2/build/_common.php
@@ -16,6 +16,12 @@ $options = array (
 		"2.0.5=R2_0_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // bug 178681
 	),
 
+	/* define a regular expression to be used to collect the most recent 
+	 * matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
+	 * and /home/www-data/build/requests/dependencies.urls.txt 
+	 * */ 	
+	"regex" => "I200.*/eclipse-SDK-|[SR]-.*200.*/eclipse-SDK-|[ISR]200.*/emf-sdo-xsd-SDK-",
+
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
 	"EmailDefault" => "jbruck@ca.ibm.com", // prefil email contact box with comma-sep'd list
