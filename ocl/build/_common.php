d971e6505eaf697fcb620516202acf75219fa20e
nickb <nickb>
2008-05-03 19:26:37 +0000

    [226437]

--- ocl/build/_common.php
+++ ocl/build/_common.php
@@ -29,7 +29,7 @@ $options = array (
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
-	"EmailDefault" => "give.a.damus@gmail.com", // prefil email contact box with comma-sep'd list
+	"EmailDefault" => "give.a.damus@gmail.com, nickboldt@gmail.com", // prefil email contact box with comma-sep'd list
 	
 	"Users" => array("nickb","nickb","nboldt") /* build user, eclipse cvs user, IES cvs user */
 );
