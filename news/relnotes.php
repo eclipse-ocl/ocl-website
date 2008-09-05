dd2932771709c6500ef3b51b71775a5f1985cc45
nickb <nickb>
2008-09-05 20:16:24 +0000

    update branch targets

--- news/relnotes.php
+++ news/relnotes.php
@@ -5,19 +5,22 @@ $pageTitle = "Eclipse Modeling - MDT - Release Notes";
 
 $streams = array(
 	"xsd" => array(
-		"2.4.x" => "HEAD",
+		"2.5.x" => "HEAD",
+		"2.4.x" => "R2_4_maintenance",
 		"2.3.x" => "R2_3_maintenance",
 		"2.2.x" => "R2_2_maintenance",
 		"2.1.x" => "R2_1_maintenance",
 		"2.0.x" => "R2_0_maintenance"
 	),
 	"ocl" => array(
-		"1.2.x" => "HEAD",
+		"1.3.x" => "HEAD",
+		"1.2.x" => "R1_2_maintenance",
 		"1.1.x" => "R1_1_maintenance",
 		"1.0.x" => "R1_0_maintenance"
 	),
 	"uml2" => array(
-		"2.2.x" => "HEAD",
+		"3.0.x" => "HEAD",
+		"2.2.x" => "R2_2_maintenance",
 		"2.1.x" => "R2_1_maintenance",
 		"2.0.x" => "R2_0_maintenance",
 		"1.1.x" => "",
