896370fc1c1b8ac74d353c41522545227c42b542
jbruck <jbruck>
2008-11-04 16:32:52 +0000

    Updates for the IES 3.4.2 maintenance builds

--- uml2/build/_common.php
+++ uml2/build/_common.php
@@ -7,25 +7,25 @@ $options = array (
 	"BranchAndJDK" => array (
 		"emf.torolab.ibm.com=------------,------------",
 		"3.0.0=HEAD,/opt/sun-java2-5.0",
-		"2.2.1=R2_2_maintenance,/opt/sun-java2-5.0",
+		"2.2.2=R2_2_maintenance,/opt/sun-java2-5.0",
 		"2.1.1=R2_1_maintenance,/opt/sun-java2-5.0",
 		"2.0.5=R2_0_maintenance,/opt/sun-java2-1.4",
 
 		"emft.eclipse.org=------------,------------",
 		"3.0.0=HEAD,/opt/sun-java2-5.0",
-		"2.2.1=R2_2_maintenance,/opt/sun-java2-5.0",
+		"2.2.2=R2_2_maintenance,/opt/sun-java2-5.0",
 		"2.1.1=R2_1_maintenance,/opt/sun-java2-5.0",
 		"2.0.5=R2_0_maintenance,/opt/sun-java2-1.4",
 
 		"modeling.eclipse.org=------------,------------",
 		"3.0.0=HEAD,/opt/sun-java2-5.0",
-		"2.2.1=R2_2_maintenance,/opt/sun-java2-5.0",
+		"2.2.2=R2_2_maintenance,/opt/sun-java2-5.0",
 		"2.1.1=R2_1_maintenance,/opt/sun-java2-5.0",
 		"2.0.5=R2_0_maintenance,/opt/sun-java2-1.4",
 
 		"build.eclipse.org=------------,------------",
 		"3.0.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
-		"2.2.1=R2_2_maintenance,/opt/public/common/ibm-java2-ppc-50",
+		"2.2.2=R2_2_maintenance,/opt/public/common/ibm-java2-ppc-50",
 		"2.1.1=R2_1_maintenance,/opt/public/common/ibm-java2-ppc-50",
 		"2.0.5=R2_0_maintenance,/opt/public/stp/apps/IBMJava2-ppc-142" // bug 178681
 	),
