4f9230b58305d3bc96f6b84cb98b1b3d61e85a8c
cdamus <cdamus>
2009-02-10 21:46:29 +0000

    Update Christian's contact information

--- ocl/build/_common.php
+++ ocl/build/_common.php
@@ -32,7 +32,7 @@ $options = array (
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
-	"EmailDefault" => "cdamus@zeligsoft.com, nickboldt@gmail.com", // prefil email contact box with comma-sep'd list
+	"EmailDefault" => "give.a.damus@gmail.com, nickboldt@gmail.com", // prefil email contact box with comma-sep'd list
 	
 	"Users" => array("cdamus","cdamus",NULL) /* build user, eclipse cvs user, IES cvs user */
 );
