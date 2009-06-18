2cd9563755e6b87646c8687d0ba219ae4ee5e181
mgolubev <mgolubev>
2009-06-18 10:02:48 +0000

    0.9.1 stream started

--- uml2tools/build/_common.php
+++ uml2tools/build/_common.php
@@ -7,12 +7,14 @@ $options = array (
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
 		"0.9.0=HEAD,/opt/sun-java2-5.0",
+		"0.9.1=R0_9_0_Maintenance,/opt/sun-java2-5.0",
 		"0.7.2=R0_7_0_Maintenance,/opt/sun-java2-5.0",
 		"0.8.1=R0_8_0_Maintenance,/opt/sun-java2-5.0",
 
 
 		"build.eclipse.org=------------,------------",
 		"0.9.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
+		"0.9.1=R0_9_0_Maintenance,/opt/public/common/ibm-java2-ppc-50",
 		"0.7.2=R0_7_0_Maintenance,/opt/public/common/ibm-java2-ppc-50",
 		"0.8.1=R0_8_0_Maintenance,/opt/public/common/ibm-java2-ppc-50"
 	),
