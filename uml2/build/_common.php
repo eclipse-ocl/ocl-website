d7a195398043b67d5a9f2ce104142044a8ecd710
nickb <nickb>
2010-05-24 15:32:36 +0000

    switch from jbruck to khussey as build publisher

--- uml2/build/_common.php
+++ uml2/build/_common.php
@@ -5,20 +5,6 @@ $options = array (
 	"BaseBuilderBranch" => "R35_M5",
 
 	"BranchAndJDK" => array (
-		"emf.torolab.ibm.com=------------,------------",
-		"3.1.0=HEAD,/opt/sun-java2-5.0",
-		"3.0.1=R3_0_maintenance,/opt/sun-java2-5.0",
-		"2.2.2=R2_2_maintenance,/opt/sun-java2-5.0",
-		"2.1.1=R2_1_maintenance,/opt/sun-java2-5.0",
-		"2.0.5=R2_0_maintenance,/opt/sun-java2-1.4",
-
-		"emft.eclipse.org=------------,------------",
-		"3.1.0=HEAD,/opt/sun-java2-5.0",
-		"3.0.1=R3_0_maintenance,/opt/sun-java2-5.0",
-		"2.2.2=R2_2_maintenance,/opt/sun-java2-5.0",
-		"2.1.1=R2_1_maintenance,/opt/sun-java2-5.0",
-		"2.0.5=R2_0_maintenance,/opt/sun-java2-1.4",
-
 		"modeling.eclipse.org=------------,------------",
 		"3.1.0=HEAD,/opt/sun-java2-5.0",
 		"3.0.1=R3_0_maintenance,/opt/sun-java2-5.0",
@@ -43,9 +29,9 @@ $options = array (
 
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
-	"EmailDefault" => "jbruck@ca.ibm.com", // prefil email contact box with comma-sep'd list
+	"EmailDefault" => "kenn.hussey@gmail.com", // prefil email contact box with comma-sep'd list
 
-	"Users" => array ("jbruck", "jbruck", "jbruck") /* build user, eclipse cvs user, IES cvs user */
+	"Users" => array ("khussey", "khussey", NULL) /* build user, eclipse cvs user, IES cvs user */
 );
 
 ?>
