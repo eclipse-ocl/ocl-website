6452492c1d321365279bb272d81d8397a8c52657
asanchez <asanchez>
2011-11-09 13:39:16 +0000

    [318090] - [releng] Introduce distinct Core and Tools builds - Firstly, updaloading the new links

--- downloads/index.php
+++ downloads/index.php
@@ -41,11 +41,17 @@ $dls = array(
 	),
 	"/ocl" => array(
 		"OCL" => array(
-			"<acronym title=\"Click to download archived All-In-One p2 Repo Update Site\"><img alt=\"Click to download archived All-In-One p2 Repo Update Site\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "Update",
-			"SDK (Runtime, EMF Edit Support, Source, Documentation)" => "SDK",
-			"Core SDK (Runtime, Source)" => "CoreSDK",
-			"Runtime" => "runtime",
-			"Examples" => "examples",
+			"<acronym title=\"Click to download archived All-In-One p2 Repo Update Site\"><img alt=\"Click to download archived All-In-One p2 Repo Update Site\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "Update",  # deprecated
+			"<acronym title=\"Click to download archived SDK P2 Repository\"><img alt=\"Click to download archived SDK P2 Repository\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">SDK Update Site</b></acronym>" => "Update-core", # new in buckminster based tools/core builds
+			"<acronym title=\"Click to download archived All-In-One Tools P2 Repository\"><img alt=\"Click to download archived All-In-One Tools P2 Repository\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "Update-tools", # new in buckminster based tools/core builds
+			"SDK (Runtime, EMF Edit Support, Source, Documentation)" => "SDK", #deprecated						
+			"SDK (Runtime, EMF Edit Support, Source, Documentation)" => "SDK-core", # new in buckminster based tools/core builds
+			"Core SDK (Runtime, Source)" => "CoreSDK", # deprecated
+			"Core SDK (Runtime, Source)" => "CoreSDK-core", # new in buckminster based tools/core builds
+			"Runtime" => "runtime", # deprecated
+			"Runtime" => "runtime-core", # new in buckminster based tools/core builds
+			"Examples" => "examples",  # deprecated
+			"Examples" => "examples-tools", # new in buckminster based tools/core builds
 			"Automated Tests" => "automated-tests",  # deprecated Modeling build 
 			"Automated Tests" => "Automated-Tests",  # new Athena build
 			"Stand-alone" => "standalone"
