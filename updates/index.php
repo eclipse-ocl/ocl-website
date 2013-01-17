f80bdda973f3c5be7fde192215eafac9248341e6
Kenn Hussey <Kenn.Hussey@gmail.com>
2013-01-16 19:16:41 -0500

    Redirecting to Modeling update site.

--- updates/index.php
+++ updates/index.php
@@ -6,13 +6,11 @@ require_once ("../../includes/buildServer-common.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/updates-common.php");
 
 $PRS = array(
-	"EMF (XSD)" => "modeling/emf"
 );
 
 function notes()
 {
-	print "<p><i style=\"color:red\"><b>NOTE:</b> For XSD, use the <a href=\"/modeling/emf/updates/\">EMF Update Site</a>.</i></p>\n";
-	print "<p><i>For MoDisco, use the <a href=\"http://wiki.eclipse.org/MoDisco/Updates\">MoDisco Update Site</a>.</i></p>\n";
+	print "<p><i>Use the <a href=\"/modeling/updates/\">Modeling Update Site</a>.</i></p>\n";
 }
 
 update_manager("MDT", "Eclipse Modeling", $PRS, true, true);
